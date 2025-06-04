0961a405575c47e31162f8e48f83fa67439b3998
Rico <rc.bilyaiz@gmail.com>
2025-06-05 02:32:39 +0800

    set-up back-end side environment

---
+++ backend/app.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import {createClient} from '@supabase/supabase-js'
+import morgan from 'morgan'
+import bodyParser from "body-parser";
+import cors from 'cors';
+
+const SUPAURL = "https://kzwkpqllpzngbcsufydb.supabase.co";
+const SUPAKEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6d2twcWxscHpuZ2Jjc3VmeWRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNDMyMTAsImV4cCI6MjA2NDYxOTIxMH0.gKRs8R1ZAfm1mw4_luhNnw8rnFe8YUP7Dvj9inFYb8E";
+const supabase = createClient(SUPAURL, SUPAKEY);
+
+const app = express();
+
+const corsOption = {
+    origin: '*',
+    methods: [ 'GET', 'POST', ],
+    allowedHeaders: [ 'Content-Type', ],
+};
+
+// using morgan for logs
+app.use(morgan('combined'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors(corsOption));
+
+app.post('/login', async (req, res) => {
+    console.log('login', req.body);
+    const { error } = await supabase.auth.signInWithPassword(req.body);
+    if (error) res.send({ success: false });
+    else res.send({ success: true });
+});
+
+app.get('/get-loggedinuser', async (req, res) => {
+    const { data } = await supabase.auth.getUser();
+    if (data.user && data.user.id) {
+        if (data.user && data.user.id) {
+            const response = await supabase
+            .from('users')
+            .select()
+            .match({ guid: data.user.id, isActive: true })
+            .single()            
+            res.send({ success: true, user: response.data });
+          } else res.send({ success: false });
+    }
+    else res.send({ success: false });
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello I am working my friend Supabase <3");
+});
+
+app.get('*', (req, res) => {
+    res.send("Hello again I am working my friend to the moon and behind <3");
+});
+
+app.listen(3001, () => {
+    console.log(`> Ready on http://localhost:3001`);
+});
