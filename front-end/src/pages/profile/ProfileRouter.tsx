1ba724c387184cd7b9be42ea936f6518f08c789f
Rico <rc.bilyaiz@gmail.com>
2025-06-04 22:07:41 +0800

    add login form

--- front-end/src/pages/profile/ProfileRouter.tsx
+++ front-end/src/pages/profile/ProfileRouter.tsx
@@ -1,11 +1,16 @@
 // components
 import Home from './view/Home';
+import Login from './view/Login';
 
 const router = [
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/login",
+    element: <Login />,
+  }
 ]
 
 export default router;
---
+++ front-end/src/pages/profile/view/Login.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+
+// componets 
+import Layout from "../../../global/Layout";
+
+// style
+import './loginStyles.scss';
+
+class ProfileOverview extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <Layout>
+        <div>
+            <div className="wrapper">
+                <div className="form-content">
+                    <h2 className="form-head"> Sign In </h2>
+                    <div className="form-body">
+                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
+                        <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
+                        <input type="submit" className="fadeIn fourth" value="Log In" />
+                    </div>
+                </div>
+            </div>
+        </div>  
+      </Layout>
+    )
+  }
+}
+
+
+
+export default ProfileOverview;
